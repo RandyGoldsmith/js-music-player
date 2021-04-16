5a8f60ebc12cbde3c5c104c2293feb835d90c626
Randy <rgoldshz@gmail.com>
2021-04-16 00:27:25 -0500

    play/pause functionality working

--- script.js
+++ script.js
@@ -1,3 +1,30 @@
+const music = document.querySelector('audio');
+const playBtn = document.getElementById('play');
+const previousBtn = document.getElementById('previous');
+const nextBtn = document.getElementById('next');
+const name = document.getElementById('track-name');
+const artist = document.getElementById('track-artist');
+const image = document.querySelector('img');
+
+let isPlaying = false;
+
+
+function playMusic() {
+    isPlaying = true;
+    playBtn.classList.replace('fa-play', 'fa-pause');
+    playBtn.setAttribute('title', 'Pause');
+    music.play();
+}
+
+function pauseMusic() {
+    isPlaying = false;
+    playBtn.classList.replace('fa-pause', 'fa-play');
+    playBtn.setAttribute('title', 'Play');
+    music.pause();
+}
+
+playBtn.addEventListener('click', () => (isPlaying ? pauseMusic() : playMusic()));
+
 const play_list = [
     {
         name: "Bio Unit",
@@ -18,3 +45,4 @@ const play_list = [
         path: "music/MarcBurt.mp3"
     }
 ]
+
