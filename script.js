7d45cb506a9709142c070cd3aedf2362553b5939
Randy <rgoldshz@gmail.com>
2021-04-15 22:24:27 -0500

    styling of music player finished

---
+++ script.js
@@ -0,0 +1,20 @@
+const play_list = [
+    {
+        name: "Bio Unit",
+        img: "img/BioUnit.jpg",
+        track: "Aerial",
+        path: "music/BioUnit.mp3"
+    },
+    {
+        name: "Eaters",
+        img: "img/Eaters.jpg",
+        track: "Tatsumaki",
+        path: "music/Eaters.mp3"
+    },
+    {
+        name: "Marc Burt",
+        img: "img/MarcBurt.jpg",
+        track: "Deep Sea",
+        path: "music/MarcBurt.mp3"
+    }
+]
