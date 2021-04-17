3be8237486a95e6c179fd1697cadca81b3125ad2
Randy <rgoldshz@gmail.com>
2021-04-16 19:20:59 -0500

    showing progress bar advancing time

--- script.js
+++ script.js
@@ -9,28 +9,13 @@ const volume_slider = document.getElementById('volume_slider');
 const volume_down = document.getElementById('down');
 const volume_up = document.getElementById('up');
 const progressContainer = document.getElementById('progress-container');
-const progress = document.getElementById('progress');
+const progress = document.getElementById('progress-bar');
+const durationEl = document.getElementById('end-time');
+const currentTimeEl = document.getElementById('current-time');
 
+// Variables 
 let isPlaying = false;
 let currentSongIndex = 0;
-
-
-function playMusic() {
-    isPlaying = true;
-    playBtn.classList.replace('fa-play', 'fa-pause');
-    playBtn.setAttribute('title', 'Pause');
-    music.play();
-}
-
-function pauseMusic() {
-    isPlaying = false;
-    playBtn.classList.replace('fa-pause', 'fa-play');
-    playBtn.setAttribute('title', 'Play');
-    music.pause();
-}
-
-playBtn.addEventListener('click', () => (isPlaying ? pauseMusic() : playMusic()));
-
 const play_list = [
     {
         name: "Bio Unit",
@@ -51,6 +36,21 @@ const play_list = [
         path: "music/MarcBurt.mp3"
     }
 ]
+
+// Functions for controls
+function playMusic() {
+    isPlaying = true;
+    playBtn.classList.replace('fa-play', 'fa-pause');
+    playBtn.setAttribute('title', 'Pause');
+    music.play();
+}
+
+function pauseMusic() {
+    isPlaying = false;
+    playBtn.classList.replace('fa-pause', 'fa-play');
+    playBtn.setAttribute('title', 'Play');
+    music.pause();
+}
 
 function loadSong(song) {
     title.textContent = song.displaySong;
@@ -85,10 +85,27 @@ function updateProgressBar(e) {
     if(isPlaying) {
         const { duration, currentTime } = e.srcElement;
         let progressBarPercentage = (currentTime / duration) * 100;
-        console.log(progressBarPercentage);
+        progress.style.width = `${progressBarPercentage}%`;
+
+        const durationMin = Math.floor(duration / 60);
+        const durationSec = Math.floor(duration % 60);
+        if(durationSec < 10) {
+            durationSec = `0${durationSec}`;
+        }
+        if(durationSec) {
+            durationEl.textContent = `${durationMin}:${durationSec}`;
+        }
+        let currentMin = Math.floor(currentTime / 60);
+        let currentSec = Math.floor(currentTime % 60);
+        if(currentSec < 10) {
+            currentSec = `0${currentSec}`;
+        }
+        currentTimeEl.textContent = `${currentMin}:${currentSec}`;
     }
 }
 
+// Event listeners 
+playBtn.addEventListener('click', () => (isPlaying ? pauseMusic() : playMusic()));
 previousBtn.addEventListener('click', previousSong);
 nextBtn.addEventListener('click', nextSong);
 music.addEventListener('timeupdate', updateProgressBar);
@@ -97,6 +114,7 @@ volume_down.addEventListener('click', () => {
     volume_slider.value = oldValue - 10
     setVolume();
 });
+
 volume_up.addEventListener('click', () => {
     let oldValue = Number(volume_slider.value);
     volume_slider.value = oldValue + 10;
